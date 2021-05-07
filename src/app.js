9ceda568c279815ea210e304aece863d7b641ac4
igreene2 <igreene@princeton.edu>
2021-05-07 12:11:15 -0400

    start screen

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@
 import { WebGLRenderer, PerspectiveCamera, Vector3 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { SeedScene } from 'scenes';
+import './app.css'
 
 // Initialize core ThreeJS components
 const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
@@ -37,6 +38,115 @@ controls.enablePan = false;
 controls.minDistance = 4;
 controls.maxDistance = 16;
 controls.update();
+
+let instructionsContainer = document.createElement('div');
+instructionsContainer.id = 'instructions-container';
+
+// Set up intro screen
+let beginContainer = document.createElement('div');
+beginContainer.id = 'begin-container';
+document.body.appendChild(beginContainer);
+
+let beginContent = document.createElement('div');
+beginContent.id = 'begin-content';
+beginContainer.appendChild(beginContent);
+
+let beginContentText = document.createElement('div');
+beginContentText.id = 'begin-text';
+beginContent.appendChild(beginContentText);
+
+let beginContentTitleText = document.createElement('h1');
+beginContentTitleText.innerText = "Turtle Trek";
+beginContentText.appendChild(beginContentTitleText);
+
+let beginContentDescription = document.createElement('p');
+beginContentDescription.innerHTML =
+    "Be a turtle! Find your babies! Don't get eaten!!";
+beginContentText.appendChild(beginContentDescription);
+
+let instructionsButton = document.createElement('div');
+instructionsButton.id = 'instructions-button';
+instructionsButton.innerHTML = 'Instructions';
+beginContent.appendChild(instructionsButton);
+
+// Set up instructions popup
+
+let instructionsContent = document.createElement('div');
+instructionsContent.id = 'instructions-content';
+instructionsContainer.appendChild(instructionsContent);
+
+let instructionsContentText = document.createElement('div');
+instructionsContentText.id = 'instructions-text';
+instructionsContent.appendChild(instructionsContentText);
+
+let instructionsTitleText = document.createElement('h1');
+instructionsTitleText.innerText = 'Instructions';
+instructionsContentText.appendChild(instructionsTitleText);
+
+let instructionsContentDescription = document.createElement('p');
+instructionsContentDescription.innerHTML =
+    "Avoid the obstacles, collect babies! Encounter a shark and you die.<br><br>" +
+    'DOWN: move down<br>' +
+    'LEFT: move left<br>' +
+    'RIGHT: move right<br>' +
+    'UP: move up<br>';
+instructionsContentText.appendChild(instructionsContentDescription);
+
+let backButton = document.createElement('div');
+backButton.id = 'back-button';
+backButton.innerHTML = 'Back';
+instructionsContent.appendChild(backButton);
+
+backButton.onclick = function () {
+    beginContainer.style.display = 'flex';
+    instructionsContainer.style.display = 'none';
+};
+
+document.body.appendChild(instructionsContainer);
+
+instructionsButton.onclick = function () {
+    beginContainer.style.display = 'none';
+    instructionsContainer.style.display = 'flex';
+};
+
+let beginContentButton = document.createElement('div');
+beginContentButton.id = 'begin-button';
+beginContentButton.innerHTML = 'BEGIN';
+beginContent.appendChild(beginContentButton);
+
+// // Set up countdown
+// var countDownDiv = document.createElement('div');
+// countDownDiv.id = 'countdown';
+// document.body.appendChild(countDownDiv);
+// let countDownNumber = document.createElement('h1');
+// countDownDiv.appendChild(countDownNumber);
+
+
+
+// Begin game
+beginContentButton.onclick = function () {
+    beginContainer.style.display = 'none';
+    // writeupContainer.style.display = 'none';
+    // countDownDiv.style.display = 'flex';
+    // let timeleft = 3;
+    // let countDownInterval = setInterval(function () {
+    //     if (timeleft < 0) {
+    //         countDownDiv.style.display = 'none';
+    //         clearInterval(countDownInterval);
+    //         countDownNumber.innerText = '';
+    //         countDownDiv.style.display = 'none';
+    //     } else if (timeleft == 0) {
+    //         countDownNumber.innerText = 'Go!';
+    //         go.play();
+    //         scene.state.newGameStarted = true;
+    //         newGameStarted = true;
+    //     } else {
+    //         countDownNumber.innerText = timeleft;
+    //         countdown.play();
+    //     }
+    //     timeleft -= 1;
+    // }, 1000);
+};
 
 // Render loop
 const onAnimationFrameHandler = (timeStamp) => {
