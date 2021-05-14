a10478ca1765e1e6cb062cf1780a9e6223b3b6c5
natsoleary <noleary@princeton.edu>
2021-05-14 16:07:08 -0400

    Updates

--- src/components/objects/Boot/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Boot } from './Boot.js';
--- src/components/objects/index.js
+++
@@ -1,7 +0,0 @@
-export * from './Turtle';
-export * from './Shark';
-export * from './Seafloor';
-export * from './TerrainPlane';
-export * from './TerrainManager';
-export * from './Baby';
-export * from './Boot';
