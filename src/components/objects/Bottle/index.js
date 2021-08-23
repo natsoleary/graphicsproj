881f332a90eb6b87f3ccc9e0979be4ac2fabd110
natsoleary <noleary@princeton.edu>
2021-08-23 16:20:34 -0700

    Updates

--- src/components/objects/Bottle/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Bottle } from './Bottle.js';
--- src/components/objects/index.js
+++
@@ -1,6 +0,0 @@
-export * from './Turtle';
-export * from './Shark';
-export * from './TerrainPlane';
-export * from './TerrainManager';
-export * from './Baby';
-export * from './Boot';
